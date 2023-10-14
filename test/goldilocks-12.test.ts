1572b9350605cd13e0fe94846e07aa60774698be
cf <cf@users.noreply.github.com>
2023-10-14 16:29:36 +0800

    Updated to use the same round constants as other (previously using custom parameters generated by sage)

--- test/goldilocks-12.test.ts
+++ test/goldilocks-12.test.ts
@@ -1,6 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { Poseidon2Goldilocks12 } from "../src/goldilocks-12";
-
+import { Poseidon2Goldilocks12, hashNoPad } from "../src/goldilocks-12";
 describe("poseidon2 goldilocks 12", () => {
   it("permute [0,0,0,0,0,0,0,0,0,0,0,0] ", () => {
     const zero = BigInt(0);
@@ -20,18 +19,18 @@ describe("poseidon2 goldilocks 12", () => {
     ];
     const permuteResult = Poseidon2Goldilocks12.permute(state);
     expect(permuteResult).toEqual([
-      BigInt("4676078130971295920"),
-      BigInt("10183258696855793571"),
-      BigInt("15600641847707049561"),
-      BigInt("6602041563876587060"),
-      BigInt("13137619883760816643"),
-      BigInt("8849590418944608963"),
-      BigInt("10998040560159952847"),
-      BigInt("15206271513183927451"),
-      BigInt("5975958540203922369"),
-      BigInt("15926261106117560013"),
-      BigInt("17396220699479397239"),
-      BigInt("16744530920792852206"),
+      BigInt("2706484646582314364"),
+      BigInt("16460758560799937193"),
+      BigInt("2052063466144512209"),
+      BigInt("9649607828149110866"),
+      BigInt("2033662915869228037"),
+      BigInt("16251664193980936151"),
+      BigInt("1869425810546879860"),
+      BigInt("399174958178618195"),
+      BigInt("963282781129924902"),
+      BigInt("348076645890865798"),
+      BigInt("8513101393627905513"),
+      BigInt("13429406292428034341"),
     ]);
   });
   it("permute [0,1,2,3,4,5,6,7,8,9,10,11] ", () => {
@@ -51,18 +50,18 @@ describe("poseidon2 goldilocks 12", () => {
     ];
     const permuteResult = Poseidon2Goldilocks12.permute(state);
     expect(permuteResult).toEqual([
-      BigInt("17095027314848075059"),
-      BigInt("18124150414442078544"),
-      BigInt("15284687323142546159"),
-      BigInt("6784844296492226711"),
-      BigInt("4870838776976620139"),
-      BigInt("6222185298486530077"),
-      BigInt("16450947849879353518"),
-      BigInt("643974238185471420"),
-      BigInt("2975392058905691406"),
-      BigInt("11609261642319894788"),
-      BigInt("11999098011033569640"),
-      BigInt("1713291167031093441"),
+      BigInt("14495112113092593719"),
+      BigInt("10329066293759305023"),
+      BigInt("4698416604356330095"),
+      BigInt("14656054275012077034"),
+      BigInt("14040445052016575643"),
+      BigInt("18281582380570098475"),
+      BigInt("6807696650297444323"),
+      BigInt("6066054412809714982"),
+      BigInt("5411921328316089755"),
+      BigInt("14958086802832817041"),
+      BigInt("4858815946660806269"),
+      BigInt("8971086601262742856"),
     ]);
   });
 });
